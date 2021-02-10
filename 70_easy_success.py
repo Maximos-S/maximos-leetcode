7a4d6bc607cd13417fe9cc9e78276d1b910d3f10
Maximos Salzman <maximossalzman@gmail.com>
2021-02-10 14:16:42 -0500

    solves leetcode 70

---
+++ 70_easy_success.py
@@ -0,0 +1,18 @@
+def climbStairs(n, memo = {}):
+    if n <= 1:
+        return 1
+    if (n - 1) not in memo:
+        k = (n - 1)
+        memo[k] = climbStairs(k, memo)
+        oneStep = memo[n - 1]
+    else:
+        oneStep = memo[n - 1]
+    if (n - 2) not in memo:
+        memo[n - 2] = climbStairs(n - 2, memo)
+        twoStep = memo[n - 2]
+    else:
+        twoStep = memo[n - 2]
+    print(oneStep + twoStep)
+    return oneStep + twoStep
+
+climbStairs(38)
